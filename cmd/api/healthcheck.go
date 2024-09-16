f08ee8aa7994eba0b94a003bd6d1c5b1df341b38
Luigi Acuna <2015112896@ub.edu.bz>
2024-09-16 14:53:21 -0400

    Added import file and mod healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,8 +11,26 @@ func (a *applicationDependencies) healthcheckHandler(w http.ResponseWriter, r *h
 	fmt.Fprintf(w, "environment: %s\n", a.config.environment)
 	fmt.Fprintf(w, "version: %s\n", appVersion)*/
 
-	jsResponse := `{"status": "avaliable", "enviroment": %q,"version": %q}`
+	/*jsResponse := `{"status": "avaliable", "enviroment": %q,"version": %q}`
 	jsResponse = fmt.Sprintf(jsResponse, a.config.environment, appVersion)
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(jsResponse))
+	w.Write([]byte(jsResponse))*/
+
+
+	data := map[string]string{
+			"status": "avalibale",
+			"enviroment": a.config.environment,
+			"version" : appVersion
+	}
+
+	jsResponse, err := json.Marshal(data)
+	if err !=nil{
+		a.logger.Error(err.Error())
+		http.Error(w,"The server encounted a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	jsResponse = append(jsResponse, '\n')
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsResponse)
 }
