5916432fe9f955e861c636f0f58251a5951d9d40
Luigi Acuna <2015112896@ub.edu.bz>
2024-10-02 15:27:42 -0400

    created json v7

---
+++ cmd/api/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (a *applicationDependencies) routes() http.Handler {
+	//setup a new router
+	router := httprouter.New()
+	//handle 404
+	router.NotFound = http.HandlerFunc(a.notFoundResponse)
+	//handle 405s
+	router.NotFound = http.HandlerFunc(a.methodNotAllowedResponse)
+	//setup routes
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
+	return router
+}
