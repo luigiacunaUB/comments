dfe9d404adfa0b5666fa89429c9345c11c968384
Luigi Acuna <2015112896@ub.edu.bz>
2024-10-16 15:06:38 -0400

    added db to code

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	//_ indicate that will not direct use of pq package
+	_ "github.com/lib/pq"
 )
 
 const appVersion = "1.0.0"
@@ -15,6 +20,9 @@ const appVersion = "1.0.0"
 type serverConfig struct {
 	port        int
 	environment string
+	db          struct {
+		dsn string
+	}
 }
 
 type applicationDependencies struct {
@@ -28,9 +36,23 @@ func main() {
 	flag.IntVar(&settings.port, "port", 4000, "Server port")
 	flag.StringVar(&settings.environment, "env", "development",
 		"Environment(development|staging|production)")
+
+	//read in the dsn
+	flag.StringVar(&settings.db.dsn, "db-dsn", "postgres://comments:fishsticks@localhost/comments?sslmode=disable", "PostgreSQL DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	db, err := openDB(settings)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	//release the database resources before exiting
+	defer db.Close()
+
+	logger.Info("database connection pool established")
 
 	appInstance := &applicationDependencies{
 		config: settings,
@@ -51,7 +73,32 @@ func main() {
 
 	logger.Info("starting server", "address", apiServer.Addr,
 		"environment", settings.environment)
-	err := apiServer.ListenAndServe()
+	err = apiServer.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(settings serverConfig) (*sql.DB, error) {
+	//open a connection pool
+	db, err := sql.Open("postgres", settings.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	//set a context to ensure DB operations dont take too long
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	//lets test if the connection pool was created
+	//we trying pinging it with a 5 second timeout
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	//return db, nil
+
+	return db, nil
 }
