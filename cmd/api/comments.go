db603ba0dbe0eea036204a3036c48de239f3e0ec
Luigi Acuna <2015112896@ub.edu.bz>
2024-10-30 00:26:06 -0400

    finished query-string-helpers

--- cmd/api/comments.go
+++ cmd/api/comments.go
@@ -202,7 +202,21 @@ func (a *applicationDependencies) deleteCommentHandler(w http.ResponseWriter, r 
 }
 
 func (a *applicationDependencies) listCommentsHandler(w http.ResponseWriter, r *http.Request) {
-	comments, err := a.commentModel.GetAll()
+	// Create a struct to hold the query parameters
+	// Later on we will add fields for pagination and sorting (filters)
+	var queryParametersData struct {
+		Content string
+		Author  string
+	}
+	// get the query parameters from the URL
+	queryParameters := r.URL.Query()
+
+	// Load the query parameters into our struct
+	queryParametersData.Content = a.getSingleQueryParameters(queryParameters, "content", "")
+
+	queryParametersData.Author = a.getSingleQueryParameters(queryParameters, "author", "")
+
+	comments, err := a.commentModel.GetAll(queryParametersData.Content, queryParametersData.Author)
 	if err != nil {
 		a.serverErrorResponse(w, r, err)
 		return
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,7 +6,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
+
+	"github.com/luigiacunaUB/comments/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -89,5 +93,41 @@ func (a *applicationDependencies) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+
+}
+
+func (a *applicationDependencies) getSingleQueryParameters(queryParameters url.Values, key string, defaultValue string) string {
+	// url.Values is a key:value hash map of the query parameters
+	result := queryParameters.Get(key)
+	if result == "" {
+		return defaultValue
+	}
+	return result
+
+}
+
+// call when we have multiple comma-separated values
+func (a *applicationDependencies) getMultipleQueryParameters(queryParameters url.Values, key string, defaultValue []string) []string {
+	result := queryParameters.Get(key)
+	if result == "" {
+		return defaultValue
+	}
+	return strings.Split(result, ",")
+
+}
+
+func (a *applicationDependencies) getSingleIntegerParameter(queryParameters url.Values, key string, defaultValue int, v *validator.Validator) int {
+	result := queryParameters.Get(key)
+	if result == "" {
+		return defaultValue
+	}
+	// try to convert to an integer
+	intValue, err := strconv.Atoi(result)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return intValue
 
 }
